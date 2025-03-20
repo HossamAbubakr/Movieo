adf3a1b3c34c9e29dc6e95d59b69cbaab14749e7
Hossam Abubakr <sam.baker990@gmail.com>
2025-03-20 11:24:42 +0200

    Server: Add pagination to controller and elasticsearch

--- server/src/controllers/movie.controller.ts
+++ server/src/controllers/movie.controller.ts
@@ -1,20 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 import { searchMovies } from "../services/elasticsearch.service";
+import { isInvalid } from "../util/validation.helper";
 
 export class MovieController {
   static async search(req: Request, res: Response, next: NextFunction): Promise<void> {
     try {
-      const searchTerm = req.query.search as string;
+      const { query, page = "1", size = "10" } = req.query;
+      const pageNum = parseInt(page as string, 10);
+      const pageSize = parseInt(size as string, 10);
 
-      if (!searchTerm?.trim() || searchTerm.trim().length < 3) {
+      if (isInvalid(query, 3)) {
         res.status(400).json({
-          message: "The 'search' query parameter is required and must be at least 3 characters long.",
+          message: "The 'query' query parameter is required and must be at least 3 characters long.",
         });
         return;
       }
 
-      const results = await searchMovies(searchTerm.trim());
-      res.json(results);
+      if (isInvalid(pageNum, 1) || isInvalid(pageSize, 1)) {
+        res.status(400).json({
+          message: "The 'page' or 'size' query parameters must be a number greater than 0.",
+        });
+        return;
+      }
+
+      const result = await searchMovies(query as string, pageNum, pageSize);
+
+      res.json(result);
     } catch (error) {
       next(error);
     }
